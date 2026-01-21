fd24cae343c05279212956c03b2e546d72ff45a8
Maks Paidych <paydich28@gmail.com>
2026-01-21 13:36:16 +0200

    Another small paddings fix

--- Sources/SSDateTimePicker/SSDateTimePicker/Views/Date picker/SSDatePicker.swift
+++ Sources/SSDateTimePicker/SSDateTimePicker/Views/Date picker/SSDatePicker.swift
@@ -52,6 +52,8 @@ public struct SSDatePicker: View, DatePickerConfigurationDirectAccess {
                 calenderContainerView
                     .padding(.all, 24)
                     .background(pickerBackgroundColor)
+                    .padding(.leading, SSPickerConstants.pickerLeadingTrailing)
+                    .padding(.trailing, SSPickerConstants.pickerLeadingTrailing)
                     .cornerRadius(pickerViewRadius)
                     .compositingGroup()
             }
